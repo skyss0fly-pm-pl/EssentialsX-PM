f0245d4ee303c71a45497c56587e6a9467e99a53
ecstsylove <131495628+iLVOEWOCK@users.noreply.github.com>
2024-04-28 13:35:27 -0700

    Update showExpSubCommand.php

--- src/ecstsy/essentialsx/Commands/SubCommands/Exp/showExpSubCommand.php
+++ src/ecstsy/essentialsx/Commands/SubCommands/Exp/showExpSubCommand.php
@@ -19,7 +19,7 @@ class showExpSubCommand extends BaseSubCommand
      */
     public function prepare(): void
     {
-        $this->setPermission("essentialsx.show-xp");
+        $this->setPermission($this->getPermission());
         $this->registerArgument(0, new RawStringArgument("player", true));
     }
 
@@ -43,5 +43,9 @@ class showExpSubCommand extends BaseSubCommand
         }
 
         $sender->sendMessage(C::colorize(str_replace(["{player}", "{xp}", "{level}", "{levelup}"], [$player->getNameTag(), number_format($player->getXpManager()->getCurrentTotalXp()), $player->getXpManager()->getXpLevel(), Utils::getExpToLevelUp($player->getXpManager()->getCurrentTotalXp())], $config->getNested("xp.show_info", "{player} §r§6has §r§c{xp} EXP §r§6(level §r§c{level}§r§6) §r§6and needs {levelup} more exp to level up."))));
+    }
+
+    public function getPermission(): string {
+        return "essentialsx.show.xp";
     }
 }

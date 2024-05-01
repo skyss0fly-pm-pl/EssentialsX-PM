9c2a334c66c8c4bbd3785d7d98bb1b3d16661130
ecstsylove <131495628+iLVOEWOCK@users.noreply.github.com>
2024-05-01 15:50:52 -0700

    Update Queries.php

--- src/ecstsy/essentialsx/Utils/Queries.php
+++ src/ecstsy/essentialsx/Utils/Queries.php
@@ -17,4 +17,11 @@ class Queries {
         public const HOMES_CREATE = "players.homes.create";
         public const HOMES_UPDATE = "players.homes.update";
         public const HOMES_DELETE = "players.homes.delete";
+
+        // WARPS QUERY 
+        public const WARPS_INIT   = "players.warps.initialize";
+        public const WARPS_SELECT = "players.warps.select";
+        public const WARPS_CREATE = "players.warps.create";
+        public const WARPS_UPDATE = "players.warps.update";
+        public const WARPS_DELETE = "players.warps.delete";
 }
